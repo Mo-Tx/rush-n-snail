52f7d5cfa1f9159c9fa9cbace0712c8ac58c6af9
MoTrix <112804896+Mo-Tx@users.noreply.github.com>
2024-08-11 15:19:10 +0100

    Delete Scenes/CameraScript.gd

--- Scenes/CameraScript.gd
+++
@@ -1,9 +0,0 @@
-class_name Camera extends Camera3D
-
-var game : Game
-
-@onready var OFFSET := position
-
-func _ready(): game = get_tree().current_scene
-
-func _process(delta): position = game.player.position + OFFSET
