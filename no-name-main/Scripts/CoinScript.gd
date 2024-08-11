0d2d5a209d58beb312126bfbfcd1d789d6e6bc47
MoTrix <112804896+Mo-Tx@users.noreply.github.com>
2024-08-11 15:18:42 +0100

    Delete no-name-main directory

--- no-name-main/Scripts/CoinScript.gd
+++
@@ -1,5 +0,0 @@
-class_name Coin extends Area3D
-
-func _on_body_entered(body): if body.is_class("CharacterBody3D"): 
-	body.collect()
-	position = Vector3(randf_range(-10, 10), position.y, randf_range(-10, 10))
